8f7b22ed77834ecee744e0bc8b41833a85747ebd
aryanteng <aryanteng892@gmail.com>
2022-10-13 03:15:32 +0530

    work

--- pages/login.js
+++ pages/login.js
@@ -4,11 +4,11 @@ import Header from "../components/common/Header";
 import Input from "../components/common/Input";
 
 function Login() {
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [dto, setDto] = useState(null);
   const onSubmit = () => {
-    if (email && password) {
+    if (email != "" && password != "") {
       setDto({ email: email, password: password });
       console.log("dto", dto);
     }
@@ -18,7 +18,7 @@ function Login() {
     <>
       <Header />
       <div className="mt-20 flex justify-center">
-        <div className="flex flex-col p-10 w-2/3 sm:w-3/5 md:w-2/5 lg:w-1/3 mt-24 shadow-lg">
+        <div className="flex flex-col p-10 w-9/12 sm:w-3/5 md:w-2/5 lg:w-1/3 mt-24 shadow-lg">
           <div className="text-center mb-5 text-lg sm:text-xl lg:text-2xl text-black">
             Login
           </div>
--- pages/signup.js
+++ pages/signup.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../components/common/Button";
+import Header from "../components/common/Header";
+import Input from "../components/common/Input";
 
 function Signup() {
-  return <div>Signup</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [dto, setDto] = useState(null);
+  const onSubmit = () => {
+    if (name != "" && email != "" && password != "") {
+      setDto({ name: name, email: email, password: password });
+      console.log("dto", dto);
+    }
+  };
+  return (
+    <>
+      <Header />
+      <div className="mt-20 flex justify-center">
+        <div className="flex flex-col p-10 w-9/12 sm:w-3/5 md:w-2/5 lg:w-1/3 mt-24 shadow-lg">
+          <div className="text-center mb-5 text-lg sm:text-xl lg:text-2xl text-black font-semibold">
+            Sign Up
+          </div>
+          <Input
+            heading={"Name"}
+            placeholder={"Name"}
+            state={name}
+            setState={setName}
+            required={true}
+          />
+          <Input
+            heading={"Email Address"}
+            placeholder={"Email"}
+            state={email}
+            setState={setEmail}
+            required={true}
+          />
+          <Input
+            heading={"Password"}
+            placeholder={"Password"}
+            state={password}
+            setState={setPassword}
+            required={true}
+          />
+          <Button type={"primary"} text={"Sign Up"} onClick={onSubmit} />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Signup;
