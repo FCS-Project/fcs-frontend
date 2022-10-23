9fc8a3d9b5985561bd10a4bbd962af87ae6cdc16
aryanteng <aryanteng892@gmail.com>
2022-10-23 18:29:20 +0530

    mui

--- pages/signup.js
+++ pages/signup.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Button from "../components/common/Button";
 import Header from "../components/common/Header";
 import Input from "../components/common/Input";
+import SEO from "../components/common/SEO";
 
 function Signup() {
   const [name, setName] = useState("");
@@ -17,6 +18,7 @@ function Signup() {
   };
   return (
     <>
+      <SEO title={"Sign Up"} />
       <Header />
       <div className="mt-20 flex justify-center">
         <div className="flex flex-col p-10 w-9/12 sm:w-3/5 md:w-2/5 lg:w-1/3 mt-24 shadow-lg">
