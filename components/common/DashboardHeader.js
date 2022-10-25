e91dce92914f563e7b6a191be6759c8153abe089
aryanteng <aryanteng892@gmail.com>
2022-10-25 19:14:59 +0530

    header

--- components/common/DashboardHeader.js
+++ components/common/DashboardHeader.js
@@ -6,11 +6,11 @@ function DashboardHeader() {
   const linkStyle =
     "text-sm md:text-md lg:text-lg transition all delay-30 hover:text-theme cursor-pointer";
   return (
-    <div className="bg-white top-0 sticky z-10 w-full px-2 md:px-3 lg:px-4 h-10 sm:h-11 md:h-12 lg:h-14 flex justify-between items-center shadow">
+    <div className="bg-white top-0 sticky z-10 w-full px-4 md:px-3 lg:px-4 h-10 sm:h-11 md:h-12 lg:h-14 flex justify-between items-center shadow">
       <Link href="/">
         <Logo />
       </Link>
-      <div className="pr-3 sm:pr-5 flex items-center flex-end gap-3.5 md:gap-4 lg:gap-5">
+      <div className="sm:pr-5 flex items-center flex-end gap-3.5 md:gap-4 lg:gap-5">
         <Link href="/">
           <p className={linkStyle}>Home</p>
         </Link>
