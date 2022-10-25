fa16dc288695a1e8b6aa869ed8a60b12e4e1ebaf
aryanteng <aryanteng892@gmail.com>
2022-10-25 16:05:38 +0530

    signup query

---
+++ utils/auth/signup.js
@@ -0,0 +1,21 @@
+import instance from "../axios";
+
+export const signup = async (dto) => {
+  return instance
+    .post("/auth/signup", dto)
+    .then((response) => {
+      console.log(response);
+      if (response.data) {
+        return {
+          access_token: response.data.access_token,
+          refresh_token: response.data.refresh_token,
+        };
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      return {
+        error: error.message,
+      };
+    });
+};
