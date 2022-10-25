ef25c1273af156e9d106ea4dfef7cff008a69c37
aryanteng <aryanteng892@gmail.com>
2022-10-25 17:34:03 +0530

    profile top

--- components/profile/ProfileTop.js
+++ components/profile/ProfileTop.js
@@ -5,11 +5,11 @@ import Button from "../common/Button";
 
 function ProfileTop() {
   return (
-    <div className="shadow-md p-10 pt-5 pb-5 flex-col content-center items-center w-4/5 ml-auto mr-auto my-10 md:w-1/3">
+    <div className="shadow-md px-8 py-3 md:py-4 md:px-8 lg:px-10 lg:py-5 flex-col items-center w-4/5 ml-auto my-6 md:my-8 lg:my-10 mr-auto md:w-1/3">
       <img
         alt="display"
         src="https://i.ibb.co/Bswp8RS/avi.jpg"
-        className="rounded-full object-cover mx-auto h-[8rem] w-[8rem] lg:h-[7rem] lg:w-[7rem]"
+        className="rounded-full object-cover mx-auto w-32 h-32 md:h-36 md:w-36 lg:h-40 lg:w-40"
       />
       <h1 className="pt-5 text-center text-xl md:text-2xl">XYZ CHOPRA</h1>
       <Link href="/edit-info">
