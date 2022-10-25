0f17e6d4be52e925ea7a0501705e5d2855ee783e
avivashishta29 <avivashishta@gmail.com>
2022-10-25 16:19:57 +0530

    create doc

--- pages/profile.js
+++ pages/profile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DashboardHeader from "../components/common/DashboardHeader";
+import CreateDoc from "../components/profile/createDoc";
 import ProfileDocs from "../components/profile/ProfileDocs";
 import ProfileTop from "../components/profile/ProfileTop";
 
@@ -9,6 +10,7 @@ function ProfilePage() {
       <DashboardHeader />
       <ProfileTop />
       <ProfileDocs />
+      <CreateDoc />
     </div>
   );
 }
