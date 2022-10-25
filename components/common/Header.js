1956a6002d300fb935303d948eb40a52b525157c
aryanteng <aryanteng892@gmail.com>
2022-10-25 19:17:09 +0530

    header

--- components/common/Header.js
+++ components/common/Header.js
@@ -8,23 +8,6 @@ function Header() {
       <Link href="/">
         <Logo />
       </Link>
-      {/* <div className="flex items-center flex-end gap-4">
-        <Link href="/">
-          <p className="transition all delay-30 hover:text-theme cursor-pointer">
-            Home
-          </p>
-        </Link>
-        <Link href="/login">
-          <p className="transition all delay-30 hover:text-theme cursor-pointer">
-            Login
-          </p>
-        </Link>
-        <Link href="/signup">
-          <p className="transition all delay-30 hover:text-theme cursor-pointer">
-            Signup
-          </p>
-        </Link>
-      </div> */}
     </div>
   );
 }
