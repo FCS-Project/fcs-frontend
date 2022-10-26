d352ea91c0dd9fc49fde9d6d92f63cd7c7454173
avivashishta29 <avivashishta@gmail.com>
2022-10-27 02:00:56 +0530

    info box

--- components/profile/ProfileInfo.js
+++ components/profile/ProfileInfo.js
@@ -1,11 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
-import PlaceIcon from "@mui/icons-material/Place";
-import LocalPhoneSharpIcon from "@mui/icons-material/LocalPhoneSharp";
 
 function ProfileInfo({ name, imgSrc, location, contact }) {
   return (
-    <div className="w-full h-64 sm:h-72 md:h-80 lg:h-96 flex flex-col justify-start overflow-hidden border-2">
+    <div className="w-full h-56 sm:h-60 md:h-72 lg:h-80 flex flex-col justify-start overflow-hidden border-2">
       <img
         alt="display picture"
         src={imgSrc ?? "https://i.ibb.co/myvq6GR/aryan.jpg"}
@@ -20,20 +18,6 @@ function ProfileInfo({ name, imgSrc, location, contact }) {
         <p className="text-xl md:text-2xl lg:text-3xl">
           {name ?? "Aryan Teng"}
         </p>
-        <div className="flex flex-row items-start sm:items-center gap-5 pb-2 sm:pb-0">
-          <div className="flex items-center">
-            <PlaceIcon className="text-theme text-sm md:text-base lg:text-lg ml-[-0.25rem] mr-0.5" />
-            <span className="text-gray-500 text-xs md:text-base">
-              {location ?? "New Delhi, Delhi - 110048"}
-            </span>
-          </div>
-          <div className="flex items-center">
-            <LocalPhoneSharpIcon className="text-theme text-sm md:text-base lg:text-lg ml-[-0.25rem] mr-1" />
-            <span className="text-gray-500 text-xs md:text-base">
-              {contact ?? "9711171503"}
-            </span>
-          </div>
-        </div>
       </div>
     </div>
   );
--- pages/user/[id].js
+++ pages/user/[id].js
@@ -5,16 +5,19 @@ import DashboardHeader from "../../components/common/DashboardHeader";
 import CreateDocButton from "../../components/profile/createDocButton";
 import Document from "../../components/common/Document";
 import ProfileInfo from "../../components/profile/ProfileInfo";
-import Description from "../../components/profile/Description";
+import InfoBox from "../../components/profile/InfoBox";
 
 function User() {
   const [document, setDocument] = useState(false);
   return (
     <>
       <DashboardHeader />
-      <div className="gap-10 ml-auto mr-auto flex flex-col justify-start w-11/12 lg:w-3/4">
+      <div className="gap-5 ml-auto mr-auto flex flex-col justify-start w-11/12 lg:w-3/4 md:gap-8 lg:gap-10">
         <ProfileInfo />
-        <Description />
+        <InfoBox location={"New Delhi, Delhi - 110048 "} />
+        <InfoBox email={"avi@iiitd.ac.in"} />
+        <InfoBox mobileNumber={"9899659815"} />
+        <InfoBox description={"hi"} />
       </div>
       {/* {!document ? (
         <CreateDocButton />
