03192a94b2372cd6e2b42875bdb2283e8ed7c4e7
aryanteng <aryanteng892@gmail.com>
2022-10-27 03:33:14 +0530

    styling

--- components/common/Search.js
+++ components/common/Search.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Input from "./Input";
 import SearchSharpIcon from "@mui/icons-material/SearchSharp";
 
 function Search({ state, setState, filter, setFilter }) {
@@ -8,7 +7,7 @@ function Search({ state, setState, filter, setFilter }) {
       <div
         className={`${
           selected ? "bg-theme text-white" : "bg-white text-theme"
-        } border-2 border-theme  text-sm w-fit py-2 px-5 my-5 cursor-pointer capitalize`}
+        } border-2 border-theme text-sm w-fit py-1 px-3 sm:py-1.5 sm:px-4 cursor-pointer capitalize`}
         onClick={() => setFilter(text)}
       >
         {text}
@@ -23,11 +22,11 @@ function Search({ state, setState, filter, setFilter }) {
         <input
           value={state}
           onChange={(e) => setState(e.target.value)}
-          className="w-full outline-none focus:border-theme  placeholder:text-xs sm:placeholder:text-base"
+          className="w-full outline-none focus:border-theme placeholder:text-xs sm:placeholder:text-base"
           placeholder={`Search by ${filter}`}
         />
       </div>
-      <div className="flex justify-start align-center gap-3">
+      <div className="flex justify-start items-center gap-3 my-5">
         <FilterType text="type" selected={filter == "type" ? true : false} />
         <FilterType text="name" selected={filter == "name" ? true : false} />
         <FilterType
