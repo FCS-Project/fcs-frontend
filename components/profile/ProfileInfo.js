c143521b0d97e7e01b010c267642bd047a38fe9a
aryanteng <aryanteng892@gmail.com>
2022-10-27 01:02:33 +0530

    everything

---
+++ components/profile/ProfileInfo.js
@@ -0,0 +1,42 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import PlaceIcon from "@mui/icons-material/Place";
+import LocalPhoneSharpIcon from "@mui/icons-material/LocalPhoneSharp";
+
+function ProfileInfo({ name, imgSrc, location, contact }) {
+  return (
+    <div className="w-full h-64 sm:h-72 md:h-80 lg:h-96 flex flex-col justify-start overflow-hidden border-2">
+      <img
+        alt="display picture"
+        src={imgSrc ?? "https://i.ibb.co/myvq6GR/aryan.jpg"}
+        className="w-full h-2/3 object-cover overflow-hidden bg-gray-200"
+      />
+      <div className="flex flex-col justify-between pl-2 gap-1">
+        <img
+          alt="display picture"
+          src={imgSrc ?? "https://i.ibb.co/myvq6GR/aryan.jpg"}
+          className="w-20 md:w-24 lg:w-28 rounded-full object-cover overflow-hidden bg-gray-200 mt-[-3.5rem] md:mt-[-4rem] lg:mt-[-5rem]"
+        />
+        <p className="text-xl md:text-2xl lg:text-3xl">
+          {name ?? "Aryan Teng"}
+        </p>
+        <div className="flex flex-row items-start sm:items-center gap-5 pb-2 sm:pb-0">
+          <div className="flex items-center">
+            <PlaceIcon className="text-theme text-sm md:text-base lg:text-lg ml-[-0.25rem] mr-0.5" />
+            <span className="text-gray-500 text-xs md:text-base">
+              {location ?? "New Delhi, Delhi - 110048"}
+            </span>
+          </div>
+          <div className="flex items-center">
+            <LocalPhoneSharpIcon className="text-theme text-sm md:text-base lg:text-lg ml-[-0.25rem] mr-1" />
+            <span className="text-gray-500 text-xs md:text-base">
+              {contact ?? "9711171503"}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileInfo;
--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,45 +1,32 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import React, { useState } from "react";
 import DashboardHeader from "../../components/common/DashboardHeader";
-import UserCard from "../../components/professional/UserCard";
-import CreateDoc from "../../components/profile/createDoc";
-import PlaceIcon from "@mui/icons-material/Place";
 import CreateDocButton from "../../components/profile/createDocButton";
 import Document from "../../components/common/Document";
+import ProfileInfo from "../../components/profile/ProfileInfo";
+import Description from "../../components/profile/Description";
 
 function User() {
   const [document, setDocument] = useState(false);
   return (
-    <div className="px-5">
+    <>
       <DashboardHeader />
-      <div className="flex-col p-5">
-        <h1 className="text-xl font-semibold my-5 lg:text-3xl">Organization</h1>
-        <div className="flex items-center">
-          <PlaceIcon className="text-theme text-xs ml-[-0.2rem]" />
-          <div className="text-xs">{"New Delhi, Delhi - 110048"}</div>
-        </div>
-        <img
-          alt="display picture"
-          src={"https://i.ibb.co/myvq6GR/aryan.jpg"}
-          className="object-cover w-full my-5 bg-gray-200 max-h-40 sm:max-h-80 lg:max-h-96"
-        />
-        <div className="text-xs text-grey mb-5 lg:text-sm">
-          Max Healthcare Institute Limited is a hospital chain based in New
-          Delhi, India. Max Healthcare owns and operates healthcare facilities
-          across the National Capital Region of Delhi, North India, and the
-          western port city of Mumbai
-        </div>
-        {!document ? (
-          <CreateDocButton />
-        ) : (
-          <>
-            <h1 className="text-xl font-semibold my-5 lg:text-3xl">
-              Your Uploaded Document
-            </h1>
-            <Document />
-          </>
-        )}
+      <div className="gap-10 ml-auto mr-auto flex flex-col justify-start w-11/12 lg:w-3/4">
+        <ProfileInfo />
+        <Description />
       </div>
-    </div>
+      {/* {!document ? (
+        <CreateDocButton />
+      ) : (
+        <>
+          <h1 className="text-xl font-semibold my-5 lg:text-3xl">
+            Your Uploaded Document
+          </h1>
+          <Document />
+        </>
+      )} */}
+    </>
   );
 }
 
