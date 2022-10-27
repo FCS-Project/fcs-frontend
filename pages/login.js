cb4af5894439346ac9b6b147e95fbb9293eb20e5
aryanteng <aryanteng892@gmail.com>
2022-10-27 14:12:38 +0530

    login

--- pages/login.js
+++ pages/login.js
@@ -38,9 +38,9 @@ function Login() {
     <>
       <SEO title={"Login"} />
       <Header />
-      <div className="mt-20 flex justify-center">
+      <div className="mt-10 flex justify-center">
         <div className="flex flex-col p-10 w-9/12 sm:w-3/5 md:w-2/5 lg:w-1/3 mt-24 shadow-lg">
-          <div className="text-center mb-2 sm:mb-5 text-lg sm:text-xl lg:text-2xl text-black">
+          <div className="text-black text-center mb-2 sm:mb-5 text-lg sm:text-xl lg:text-2xl text-black">
             Login
           </div>
           <Input
@@ -50,6 +50,7 @@ function Login() {
             setState={setEmail}
             required={true}
           />
+          <div className="my-1 sm:my-1.5"></div>
           <Input
             heading={"Password"}
             type={"password"}
@@ -58,7 +59,9 @@ function Login() {
             setState={setPassword}
             required={true}
           />
-          <Button type={"primary"} text={"Login"} onClick={onSubmit} />
+          <div className="my-5">
+            <Button type={"primary"} text={"Login"} onClick={onSubmit} />
+          </div>
           <div className="text-sm md:text-base">
             Not a User?{" "}
             <span className="text-theme hover:underline">
