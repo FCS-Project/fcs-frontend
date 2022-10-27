5c896675f42599733e5b811638b2c088518a0e24
aryanteng <aryanteng892@gmail.com>
2022-10-27 13:15:09 +0530

    😚

--- components/common/Search.js
+++ components/common/Search.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchSharpIcon from "@mui/icons-material/SearchSharp";
+import FilterListSharpIcon from "@mui/icons-material/FilterListSharp";
 
 function Search({ state, setState, filter, setFilter }) {
   function FilterType({ text, selected }) {
@@ -7,7 +8,7 @@ function Search({ state, setState, filter, setFilter }) {
       <div
         className={`${
           selected ? "bg-theme text-white" : "bg-white text-theme"
-        } border-2 border-theme text-sm w-fit py-1 px-3 sm:py-1.5 sm:px-4 cursor-pointer capitalize`}
+        } border-2 border-theme text-xs sm:text-sm w-fit py-1 px-3 sm:py-1.5 sm:px-4 cursor-pointer capitalize`}
         onClick={() => setFilter(text)}
       >
         {text}
@@ -18,7 +19,7 @@ function Search({ state, setState, filter, setFilter }) {
   return (
     <div className="m-5 mb-0">
       <div className="flex justify-start items-center w-full border-2 p-1 br-4">
-        <SearchSharpIcon className="text-theme mx-2" />
+        <SearchSharpIcon className="text-theme text-lg sm:text-xl mx-1 sm:mx-2" />
         <input
           value={state}
           onChange={(e) => setState(e.target.value)}
@@ -27,6 +28,7 @@ function Search({ state, setState, filter, setFilter }) {
         />
       </div>
       <div className="flex justify-start items-center gap-3 my-5">
+        <FilterListSharpIcon className="text-theme text-3xl sm:text-4xl" />
         <FilterType text="type" selected={filter == "type" ? true : false} />
         <FilterType text="name" selected={filter == "name" ? true : false} />
         <FilterType
