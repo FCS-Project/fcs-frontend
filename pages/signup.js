5247a2929ea01c44d4d06de016d1721f91415fc9
avivashishta29 <avivashishta@gmail.com>
2022-10-28 03:52:23 +0530

    signup as org or patient

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Button from "../components/common/Button";
 import Header from "../components/common/Header";
 import Input from "../components/common/Input";
@@ -10,15 +10,33 @@ function Signup() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [professional, setProfessional] = useState(false);
-  const [patient, setPatient] = useState(false);
+  const [location, setLocation] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [organistionType, setOrganistionType] = useState("");
+  const [userType, setUserType] = useState("");
+
+  const [organisationFlag, setOrganizationFlag] = useState(false);
+
+  const chooseDPButton = useRef(null);
+  const chooseBannerButton = useRef(null);
+
+  const handleDPClick = () => {
+    chooseDPButton.current.click();
+  };
+
+  const handleBannerClick = () => {
+    chooseBannerButton.current.click();
+  };
+
   const onSubmit = () => {
     if (name != "" && email != "" && password != "") {
       const dto = {
         name: name,
         email: email,
         password: password,
-        roles: [patient ? "Patient" : "Professional"],
+        roles: [organisationFlag ? "Organisation" : "User"],
+        type: [organisationFlag ? organistionType : userType],
       };
       console.log("dto", dto);
     }
@@ -28,10 +46,14 @@ function Signup() {
     <>
       <SEO title={"Sign Up"} />
       <Header />
-      <div className="mt-10 flex justify-center">
-        <div className="flex flex-col p-10 w-10/12 sm:w-4/5 md:w-3/5 lg:w-2/5 mt-24 shadow-lg">
+      <div className="flex justify-center items-center ">
+        <div
+          className={`flex flex-col p-10 w-11/12 sm:w-4/5 md:w-3/5 lg:w-2/5 shadow-lg ${
+            organisationFlag ? " mt-7" : " mt-24"
+          }`}
+        >
           <div className="text-center mb-2 sm:mb-5 text-lg sm:text-xl lg:text-2xl">
-            Sign Up
+            Sign Up as {organisationFlag ? "an Organisation" : "a User"}
           </div>
           <Input
             heading={"Name"}
@@ -56,39 +78,141 @@ function Signup() {
             setState={setPassword}
             required={true}
           />
+
+          {organisationFlag && (
+            <>
+              <Input
+                heading={"Description"}
+                type={"text"}
+                placeholder={"Description"}
+                state={description}
+                setState={setDescription}
+                required={true}
+              />
+              <Input
+                heading={"Location"}
+                type={"text"}
+                placeholder={"Location"}
+                state={location}
+                setState={setLocation}
+                required={true}
+              />
+              <div className="mt-2 text-sm sm:text-base lg:text-lg">
+                Upload Pictures
+              </div>
+              <div className="flex w-full items-center justify-between mt-2 gap-2 lg:gap-5">
+                <input
+                  type="file"
+                  name="dp"
+                  className="hidden"
+                  ref={chooseDPButton}
+                ></input>
+                <Button
+                  text="Profile Pic"
+                  type="secondary"
+                  onClick={() => {
+                    handleDPClick();
+                  }}
+                />
+                <input
+                  type="file"
+                  name="banner"
+                  className="hidden"
+                  ref={chooseBannerButton}
+                ></input>
+                <Button
+                  text="Banner Pic"
+                  type="secondary"
+                  onClick={() => {
+                    handleBannerClick();
+                  }}
+                />
+              </div>
+            </>
+          )}
           <div className="flex items-center my-4">
-            <div className="mr-3">
-              <input
-                id="patient"
-                type="checkbox"
-                name="patient"
-                value={patient}
-                checked={patient}
-                onClick={() => {
-                  setProfessional(false);
-                  setPatient(!patient);
-                }}
-              />
-              <label for="patient" className={labelStyle}>
-                Patient
-              </label>
-            </div>
-            <div>
-              <input
-                id="professional"
-                type="checkbox"
-                name="professional"
-                value={professional}
-                checked={professional}
-                onClick={() => {
-                  setPatient(false);
-                  setProfessional(!professional);
-                }}
-              />
-              <label for="professional" className={labelStyle}>
-                Professional
-              </label>
-            </div>
+            {organisationFlag ? (
+              <>
+                <div className="mr-3 flex items-center">
+                  <input
+                    id="hospital"
+                    type="checkbox"
+                    name="hospital"
+                    value={organistionType}
+                    checked={organistionType == "hospital"}
+                    onClick={() => {
+                      setOrganistionType("hospital");
+                    }}
+                  />
+                  <label for="hospital" className={labelStyle}>
+                    Hospital
+                  </label>
+                </div>
+                <div className="mr-3 flex items-center">
+                  <input
+                    id="pharmacy"
+                    type="checkbox"
+                    name="pharmacy"
+                    value={organistionType}
+                    checked={organistionType == "pharmacy"}
+                    onClick={() => {
+                      setOrganistionType("pharmacy");
+                    }}
+                  />
+                  <label for="pharmacy" className={labelStyle}>
+                    Pharmacy
+                  </label>
+                </div>
+                <div>
+                  <input
+                    id="insurance"
+                    type="checkbox"
+                    name="insurance"
+                    value={organistionType}
+                    checked={organistionType == "insurance"}
+                    onClick={() => {
+                      setOrganistionType("insurance");
+                    }}
+                  />
+                  <label for="insurance" className={labelStyle}>
+                    Insurance
+                  </label>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="mr-3 flex items-center">
+                  <input
+                    id="patient"
+                    type="checkbox"
+                    name="patient"
+                    value={userType}
+                    checked={userType == "patient"}
+                    onClick={() => {
+                      setUserType("patient");
+                    }}
+                  />
+                  <label for="patient" className={labelStyle}>
+                    Patient
+                  </label>
+                </div>
+                <div>
+                  <input
+                    id="professional"
+                    type="checkbox"
+                    name="professional"
+                    value={userType}
+                    checked={userType == "professional"}
+                    onClick={() => {
+                      setUserType("professional");
+                    }}
+                  />
+                  <label for="professional" className={labelStyle}>
+                    Professional
+                  </label>
+                </div>
+              </>
+            )}
           </div>
           <Button type={"primary"} text={"Sign Up"} onClick={onSubmit} />
           <div className="text-sm md:text-base mt-4">
@@ -97,6 +221,27 @@ function Signup() {
               <Link href="/login">Login</Link>
             </span>
           </div>
+          {organisationFlag ? (
+            <div className="text-sm md:text-base mt-4">
+              Want to sign up as a User?{" "}
+              <span
+                className="text-theme hover:underline cursor-pointer"
+                onClick={() => setOrganizationFlag(false)}
+              >
+                Click Here.
+              </span>
+            </div>
+          ) : (
+            <div className="text-sm md:text-base mt-4">
+              Want to sign up as an Organization?{" "}
+              <span
+                className="text-theme hover:underline cursor-pointer"
+                onClick={() => setOrganizationFlag(true)}
+              >
+                Click Here.
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </>
