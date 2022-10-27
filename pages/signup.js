522547825719c99e5ec1d2293e8ce248cc6837b5
aryanteng <aryanteng892@gmail.com>
2022-10-27 12:56:10 +0530

    styling

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Button from "../components/common/Button";
 import Header from "../components/common/Header";
 import Input from "../components/common/Input";
