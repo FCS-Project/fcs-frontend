3376449b11be6bf02a0d0a6dc5c7d787005498c8
aryanteng <aryanteng892@gmail.com>
2022-10-27 15:09:05 +0530

    😔

--- pages/login.js
+++ pages/login.js
@@ -39,7 +39,7 @@ function Login() {
       <SEO title={"Login"} />
       <Header />
       <div className="mt-10 flex justify-center">
-        <div className="flex flex-col p-10 w-9/12 sm:w-3/5 md:w-2/5 lg:w-1/3 mt-24 shadow-lg">
+        <div className="flex flex-col p-10 w-10/12 sm:w-4/5 md:w-3/5 lg:w-2/5 mt-24 shadow-lg">
           <div className="text-black text-center mb-2 sm:mb-5 text-lg sm:text-xl lg:text-2xl text-black">
             Login
           </div>
--- pages/signup.js
+++ pages/signup.js
@@ -29,7 +29,7 @@ function Signup() {
       <SEO title={"Sign Up"} />
       <Header />
       <div className="mt-10 flex justify-center">
-        <div className="flex flex-col p-10 w-9/12 sm:w-3/5 md:w-2/5 lg:w-1/3 mt-24 shadow-lg">
+        <div className="flex flex-col p-10 w-10/12 sm:w-4/5 md:w-3/5 lg:w-2/5 mt-24 shadow-lg">
           <div className="text-center mb-2 sm:mb-5 text-lg sm:text-xl lg:text-2xl text-black">
             Sign Up
           </div>
