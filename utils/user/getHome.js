d58d32db056b9368eacb24742950272f0736114a
aryanteng <aryanteng892@gmail.com>
2022-10-29 02:12:07 +0530

    home page data request

--- utils/user/getHome.js
+++ utils/user/getHome.js
@@ -1,3 +1,21 @@
+import instance from "../../axios";
+import { getAccessToken } from "../../lib/auth";
+
 export const getHome = async () => {
-  return inst;
+  const jwt = getAccessToken();
+  return instance
+    .get("/user/home", { headers: { Authorization: `Bearer ${jwt}` } })
+    .then((response) => {
+      if (response.data.success) {
+        return {
+          success: response.data.success,
+          response: response.data.data,
+        };
+      }
+    })
+    .catch((error) => {
+      return {
+        error: error.message,
+      };
+    });
 };
