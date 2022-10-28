479881447ff08da01e5f07a79943dfdb654fe5f0
aryanteng <aryanteng892@gmail.com>
2022-10-28 23:07:13 +0530

    changes

--- utils/auth/signup.js
+++ utils/auth/signup.js
@@ -4,9 +4,7 @@ export const signup = async (dto) => {
   return instance
     .post("/auth/signup", dto)
     .then((response) => {
-      console.log(response);
       if (response.data) {
-        localStorage.setItem("user", JSON.stringify(response.data));
         return {
           access_token: response.data.access_token,
           refresh_token: response.data.refresh_token,
