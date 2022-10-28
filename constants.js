bec010e0276cd5d77f5d218674019421dc55a6e7
aryanteng <aryanteng892@gmail.com>
2022-10-29 05:26:20 +0530

    auth

--- constants.js
+++ constants.js
@@ -2,6 +2,7 @@ export const BASE_API_URL = "http://localhost:5001";
 
 export const SIGNUP = "/auth/signup";
 export const SIGNIN = "/auth/signin";
+export const LOGOUT = "/auth/logout";
 export const VERIFY_OTP = "/auth/verify-otp";
 export const REFRESH_TOKEN = "/auth/refresh";
 
