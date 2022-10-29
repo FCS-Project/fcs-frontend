ac53c74359b171ca132b9a18f5fd0b2c01e33626
aryanteng <aryanteng892@gmail.com>
2022-10-29 17:47:15 +0530

    search

--- components/profile/createDocForm.js
+++ components/profile/createDocForm.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef, useState } from "react";
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useState } from "react";
 import uploadImage from "../../utils/imageUpload";
 import Button from "../common/Button";
 import Input from "../common/Input";
@@ -6,17 +7,14 @@ import Input from "../common/Input";
 function CreateDocForm() {
   const [name, setName] = useState("");
   const [type, setType] = useState("");
-
   const [file, setFile] = useState("");
   const [fileSrc, setFileSrc] = useState("");
 
   const handleFileChange = async (changeEvent) => {
     const reader = new FileReader();
-
     reader.onload = function (onLoadEvent) {
       setFile(onLoadEvent.target.result);
     };
-
     reader.readAsDataURL(changeEvent.target.files[0]);
     await uploadImage(changeEvent, setFileSrc);
   };
@@ -35,7 +33,6 @@ function CreateDocForm() {
         state={type}
         setState={setType}
       />
-
       <div className="my-2 sm:my-5">
         <form method="post" onChange={(event) => handleFileChange(event)}>
           <label htmlFor="file">
@@ -43,17 +40,16 @@ function CreateDocForm() {
               Upload File
             </div>
           </label>
-
           {fileSrc && (
             <>
               <p className="text-sm mt-5">Image Uploaded</p>
               <img
+                alt="image"
                 src={file}
                 className="w-fit max-w-1/3 block ml-auto mr-auto border-theme my-5"
               />
             </>
           )}
-
           <input
             type="file"
             id="file"
