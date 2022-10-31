cbd172b5831af64981ac38d5b883616dd51fa640
aryanteng <aryanteng892@gmail.com>
2022-10-31 14:07:53 +0530

    get shared documents

---
+++ utils/document/getSharedDocuments.js
@@ -0,0 +1,19 @@
+import instance from "../../axios";
+
+export const getUserDocuments = async () => {
+  return instance
+    .get("/document/shared")
+    .then((response) => {
+      if (response.data.success) {
+        return {
+          success: response.data.success,
+          data: response.data.data,
+        };
+      }
+    })
+    .catch((error) => {
+      return {
+        error: error.message,
+      };
+    });
+};
