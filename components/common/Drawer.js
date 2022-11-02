c9261f2164384338718fb34fa433a9e79fed74c9
aryanteng <aryanteng892@gmail.com>
2022-11-02 23:32:16 +0530

    personalised user drawer

--- components/common/Drawer.js
+++ components/common/Drawer.js
@@ -1,16 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 import { useState } from "react";
 import Drawer from "@mui/material/Drawer";
 import MenuIcon from "@mui/icons-material/Menu";
-import { IconButton } from "@mui/material";
+import { Avatar, Divider, IconButton, List } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { logout } from "../../store/actions/auth";
 import { emptyUser } from "../../store/actions/user";
+import { ListItem, ListItemButton, ListItemText } from "@mui/material";
 
 export default function DrawerComponent() {
   const [open, setOpen] = useState(false);
-  const user = useSelector((state) => state.user.data);
+  const user = useSelector((state) => state.user);
   const auth = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const router = useRouter();
@@ -24,22 +26,33 @@ export default function DrawerComponent() {
   const linkStyle =
     "text-sm md:text-md lg:text-lg transition all delay-30 hover:text-theme cursor-pointer mb-5";
 
+  const navItems = [
+    { page: "Home", href: "/" },
+    { page: "Shared Documents", href: "/documents" },
+    { page: "Shop", href: "/" },
+    { page: "Profile", href: "/profile" },
+  ];
+
   return (
     <div>
-      {user && (
+      {user?.data && (
         <IconButton onClick={() => setOpen(true)}>
           <MenuIcon className="text-black" />
         </IconButton>
       )}
       <Drawer anchor={"right"} open={open} onClose={() => setOpen(false)}>
-        <div className="w-64 p-5">
+        <div className="w-64 pt-5">
           {user && (
             <>
-              {user?.roles[0] == "Admin" ? (
+              {user?.data?.roles[0] == "Admin" ? (
                 <>
-                  <Link href="/" passHref={true}>
-                    <p className={linkStyle}>Home</p>
-                  </Link>
+                  {navItems.map((item, i) => {
+                    return (
+                      <Link key={item} href={item.href} passHref={true}>
+                        <p className={linkStyle}>Home</p>
+                      </Link>
+                    );
+                  })}
                   <Link href="/users" passHref={true}>
                     <p className={linkStyle}>Users</p>
                   </Link>
@@ -54,23 +67,33 @@ export default function DrawerComponent() {
                   </p>
                 </>
               ) : (
-                <>
-                  <Link href="/" passHref={true}>
-                    <p className={linkStyle}>Home</p>
-                  </Link>
-                  <Link href="/documents" passHref={true}>
-                    <p className={linkStyle}>Shared Documents</p>
-                  </Link>
-                  <Link href="/shop" passHref={true}>
-                    <p className={linkStyle}>Shop</p>
-                  </Link>
-                  <Link href="/profile" passHref={true}>
-                    <p className={linkStyle}>Profile</p>
-                  </Link>
-                  <p className={linkStyle} onClick={() => onClick()}>
-                    Logout
-                  </p>
-                </>
+                <div className="text-center">
+                  <div className="flex flex-col items-center gap-2 mb-3">
+                    <img
+                      alt="display"
+                      src={
+                        user?.data?.displaySrc == "" || !user?.data?.displaySrc
+                          ? "/user.png"
+                          : displaySrc
+                      }
+                      className="w-20 h-20 rounded-full object-cover border-2 border-theme"
+                    />
+                    <div className="text-theme">{user.data?.name}</div>
+                  </div>
+                  <Divider />
+                  <div className="mt-5">
+                    {navItems.map((item, i) => {
+                      return (
+                        <Link key={i} href={item.href} passHref={true}>
+                          <p className={linkStyle}>{item.page}</p>
+                        </Link>
+                      );
+                    })}
+                    <p className={linkStyle} onClick={() => onClick()}>
+                      Logout
+                    </p>
+                  </div>
+                </div>
               )}
             </>
           )}
