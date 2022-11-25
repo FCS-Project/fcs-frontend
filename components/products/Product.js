5ee17ae5b084f8ba9c38555da0010ff03f6a0e24
avivashishta29 <avivashishta@gmail.com>
2022-11-26 01:49:31 +0530

    razorpay

--- components/products/Product.js
+++ components/products/Product.js
@@ -1,8 +1,66 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useCallback } from "react";
 import Button from "../common/Button";
+import useRazorpay from "react-razorpay";
 
 function Product({ id, name, price, owner }) {
+  const razorpayInstance = useRazorpay({
+    authKey: {
+      key_id: "rzp_test_ZiVolzO6sax6X4",
+      key_secret: "NWiFcQYN3sqtdcawncbg5Vaz",
+    },
+  });
+
+  const openRazorpay = async () => {
+    const order = await razorpayInstance.orders.create({
+      amount: 111,
+      currency: "INR",
+    });
+
+    const options = {
+      key: "rzp_test_ZiVolzO6sax6X4",
+      amount: 100,
+      currency: "INR",
+      name: "VAMA CARE",
+      description: "Proceed to buy this product",
+      image: "https://i.ibb.co/Ct1jrgj/Logo2.png",
+      order_id: order.id,
+      handler: (res) => {
+        console.log(res);
+        //  Call dispatch
+      },
+      prefill: {
+        name: "Avi",
+        email: "email",
+        contact: "phone",
+      },
+      notes: {
+        address: "VAMA CARE OFFICE",
+      },
+      theme: {
+        color: "var(--black)",
+      },
+      callback_url: "",
+    };
+
+    const rzpay = new razorpayInstance(options);
+    rzpay.open();
+
+    rzpay.on("payment.failed", function (response) {
+      console.log(response.error.code);
+      console.log(response.error.description);
+      console.log(response.error.source);
+      console.log(response.error.step);
+      console.log(response.error.reason);
+      console.log(response.error.metadata.order_id);
+      console.log(response.error.metadata.payment_id);
+    });
+
+    rzpay.on("payment.success", (res) => {
+      console.log("WOHOOOO", res);
+    });
+  };
+
   return (
     <div className="mx-0 md:w-[30%] lg:w-1/5 p-2 md:p-2 md:mx-0 lg:p-4 flex flex-col gap-2 lg:py-3 shadow-lg my-5 h-fit">
       <div className="flex jusitfy-start items-center gap-2 text-md lg:text-lg capitalise">
@@ -31,7 +89,7 @@ function Product({ id, name, price, owner }) {
         <div className="text-md lg:text-lg capitalise">${price ?? "299"}</div>
       </div>
       <div className="flex justify-start items-center gap-1">
-        <Button text="Buy" type="tertiary" onClick={() => alert("bought :)")} />
+        <Button text="Buy" type="tertiary" onClick={() => alert("bought")} />
       </div>
     </div>
   );
