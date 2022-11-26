dcbfc4f3086eb7253909e270542e3ffd9f1c07e2
avivashishta29 <avivashishta@gmail.com>
2022-11-26 16:03:45 +0530

    create product

---
+++ pages/create-product.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import Button from "../components/common/Button";
+import Header from "../components/common/Header";
+import Input from "../components/common/Input";
+import uploadImage from "../utils/image/imageUpload";
+
+function CreateProductPage() {
+  const [productName, setProductName] = useState("");
+  const [price, setPrice] = useState("");
+  const [productDP, setProductDP] = useState("");
+  const [productDPSrc, setProductDPSrc] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleFileChange = async (changeEvent) => {
+    setLoading(true);
+    const reader = new FileReader();
+    reader.onload = function (onLoadEvent) {
+      setProductDP(onLoadEvent.target.result);
+    };
+    reader.readAsDataURL(changeEvent.target.files[0]);
+    const upload = await uploadImage(changeEvent, setProductDPSrc, "file");
+    setLoading(false);
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="flex justify-center items-center">
+        <div
+          className={`flex flex-col p-10 w-11/12 sm:w-4/5 md:w-3/5 lg:w-1/2 shadow-lg mt-10`}
+        >
+          <div
+            className={`text-center mb-2 sm:mb-5 text-lg sm:text-xl lg:text-2xl mb-2`}
+          >
+            Create A Product
+          </div>
+          {/* {error && (
+            <div className="text-theme text-center">Error: {error}</div>
+          )} */}
+          <Input
+            heading={"Product Name"}
+            placeholder={"Name"}
+            state={productName}
+            setState={setProductName}
+            required={true}
+            style="my-0.5"
+          />
+          <Input
+            heading={"Product Price In INR"}
+            placeholder={"Price"}
+            state={price}
+            setState={setPrice}
+            required={true}
+            style="my-0.5"
+          />
+
+          <div className="my-1">
+            <div className="text-sm sm:text-base lg:text-lg">
+              Upload Product Picture
+            </div>
+            <div className="flex w-full items-start justify-between mt-2 gap-2 lg:gap-5">
+              <form
+                method="post"
+                onChange={(event) => {
+                  handleFileChange(event);
+                }}
+                className="w-full"
+              >
+                <label htmlFor="file">
+                  <div className="w-full py-1.5 text-sm sm:text-base hover:opacity-90 bg-white text-theme border-2 border-theme text-center">
+                    Click To Upload
+                  </div>
+                </label>
+
+                {productDPSrc && (
+                  <>
+                    <p className="text-sm mt-5">Image Uploaded</p>
+                    <img
+                      alt="display picture"
+                      src={productDP}
+                      className="w-1/2 max-h-[10rem] object-contain max-w-1/2 block ml-auto mr-auto border-theme my-5"
+                    />
+                  </>
+                )}
+                {loading && <p className="text-sm mt-5">Image Loading...</p>}
+
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  accept=".png,.jpg,.jpeg,.webp"
+                  style={{ display: "none" }}
+                />
+              </form>
+            </div>
+          </div>
+          <div className="mt-2" />
+          <Button
+            type={"primary"}
+            text={"Create Product"}
+            style={"mb-4"}
+            onClick={() => {
+              alert("creating product");
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateProductPage;
--- pages/shop.js
+++ pages/shop.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import Header from "../components/common/Header";
 import Loader from "../components/common/Loader";
@@ -27,6 +28,11 @@ function ShopPage() {
     <div>
       <Header />
       <ProductFlex products={products} />
+      <Link href="/create-product" passHref={true}>
+        <div className="p-6 md:p-7 lg:p-8 w-3 h-3 sm:h-6 sm:w-6 lg:h-12 lg:w-12 bg-theme fixed bottom-0 right-0 m-2 flex items-center justify-center text-white text-3xl sm:text-4xl rounded-full cursor-pointer shadow-md">
+          +
+        </div>
+      </Link>
     </div>
   );
 }
