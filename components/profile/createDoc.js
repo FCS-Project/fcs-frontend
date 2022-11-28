6e90574cb3094e5667c603c2ff1a2f055b6c9b5f
avivashishta29 <avivashishta@gmail.com>
2022-11-28 16:51:21 +0530

    loader on doc uplaod

--- components/profile/createDoc.js
+++ components/profile/createDoc.js
@@ -4,14 +4,17 @@ import { encrypt } from "../../utils/crypto/encryption";
 import { postDocument } from "../../utils/document/postDocument";
 import Button from "../common/Button";
 import CreateDocForm from "./createDocForm";
+import { BounceLoader } from "react-spinners";
 
 function CreateDoc({ shareId }) {
   const [modal, setModal] = useState(false);
   const [file, setFile] = useState("");
   const [name, setName] = useState("");
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const user = useSelector((state) => state.user.data);
   const submit = () => {
+    setLoading(true);
     if (file) {
       let dataURI = file.split(";base64,").pop();
       postDocument({
@@ -19,14 +22,30 @@ function CreateDoc({ shareId }) {
         userId: user?.id,
         name: name,
         sharedWith: shareId,
+      }).then((response) => {
+        if (response.success) {
+          setLoading(false);
+          setModal(false);
+          setError("");
+          setFile("");
+        } else {
+          setError(response.error);
+          setFile("");
+          setLoading(false);
+        }
       });
-      setModal(false);
-      setError("");
     } else {
       setError("Please Upload A File First!");
     }
   };
 
+  if (loading) {
+    return (
+      <div className="fixed z-10 left-0 top-0 overflow-auto w-full h-full bg-[rgb(0,0,0,0.8)] flex items-center justify-center">
+        <BounceLoader height={8} width={200} color={"var(--theme)"} />
+      </div>
+    );
+  }
   return (
     <>
       {modal && (
